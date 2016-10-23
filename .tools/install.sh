cfe501be71e21f448019cc132755de382caf19bb
Kutsan Kaplan <me@kutsankaplan.com>
2016-10-23 22:55:22 +0300

    Installation script updated

--- .tools/install.sh
+++ .tools/install.sh
@@ -5,17 +5,29 @@ rm .zshrc
 if [[ $(uname -o) = "Android" ]]; then
 	apt update
 	apt install -y git zsh vim tmux nodejs tree man
+
+	# Oh My Zsh
 	sh -c "$(curl -fsSL https://raw.github.com/skeevy420/oh-my-zsh/skeevy420-termux/tools/install.sh)"
+
+	termux-setup-storage
+
 elif [[ $(uname) = "Linux" ]]; then
 	sudo apt-get update
 	sudo apt-get -y install git zsh vim tmux nodejs npm tree nmap
+
+	# Oh My Zsh
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 elif [[ $(uname) = "Darwin" ]]; then
 	if ! which brew > /dev/null; then
 		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi
 
 	brew install git zsh vim tmux node git-extras gnupg tree trash
+
+	# Oh My Zsh
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 else
 	echo
 	echo 'Something happened.'
