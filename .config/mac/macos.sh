3222d616d2addfe3fb21bcfafe86f8aea015d8e4
Kutsan Kaplan <me@kutsankaplan.com>
2017-03-08 22:51:15 +0300

    [.config/mac] Update Mac defaults options

--- .config/mac/macos.sh
+++ .config/mac/macos.sh
@@ -1,5 +1,20 @@
-# Dock
+#!/usr/bin/env bash
 
-defaults write com.apple.dock tilesize -int 39;
+# -- Global {{{1
+# --------------------------------------------------------------------------------------------------
 
-killall Dock;
+# Show all files everywhere. Even in open dialog.
+defaults write -g AppleShowAllFiles -bool true
+
+# Don't write .DS_Store files to portable storages
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# -- Dock {{{1
+# --------------------------------------------------------------------------------------------------
+
+# Fully resize Dock's body.
+defaults write com.apple.dock tilesize -int 39
+
+killall Dock
+
+# }}} vim: foldmethod=marker : foldlevel=0
